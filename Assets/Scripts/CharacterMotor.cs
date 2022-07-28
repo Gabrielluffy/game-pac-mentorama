ea193d1f740b9c06301bebbdcd9ac9ba83d608a4
Gabriel <gabrielloiola20@hotmail.com>
2022-07-28 19:31:51 -0300

    Movimentacao do pacman

---
+++ Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,158 @@
+using System;
+using UnityEngine;
+
+public enum Direction
+{
+	None,
+	Up,
+	Left,
+	Down,
+	Right
+}
+
+public class CharacterMotor : MonoBehaviour
+{
+
+	public float MoveSpeed;
+
+	private Vector2 _currentMovementDirection;
+	private Vector2 _desireMovementDirection;
+
+	private Rigidbody2D _rigidbody;
+
+	private Vector2 _boxSize;
+
+	public event Action<Direction> OnDirectionCharged;
+
+	public Direction CurrentMoveDirection
+	{
+		get
+		{
+			//Up
+			if (_currentMovementDirection.y > 0)
+			{
+				return Direction.Up;
+			}
+			//Left
+			if (_currentMovementDirection.x < 0)
+			{
+				return Direction.Left;
+			}
+			//Down
+			if (_currentMovementDirection.y < 0)
+			{
+				return Direction.Down;
+			}
+			//Right
+			if (_currentMovementDirection.x > 0)
+			{
+				return Direction.Right;
+			}
+			return Direction.None;
+		}
+
+	}
+
+	public void SetMoveDirection(Direction newMoveDirection)
+	{
+		switch (newMoveDirection)
+		{
+			default:
+			case Direction.None:
+				break;
+			case Direction.Up:
+				_desireMovementDirection = Vector2.up;
+				break;
+			case Direction.Left:
+				_desireMovementDirection = Vector2.left;
+				break;
+			case Direction.Down:
+				_desireMovementDirection = Vector2.down;
+				break;
+			case Direction.Right:
+				_desireMovementDirection = Vector2.right;
+				break;
+		}
+	}
+
+	private void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_boxSize = GetComponent<BoxCollider2D>().size;
+	}
+
+	private void Update()
+	{
+
+	}
+
+	private void FixedUpdate()
+	{
+		float moveDistance = MoveSpeed * Time.fixedDeltaTime;
+		var nextMovePosition = _rigidbody.position + _currentMovementDirection * MoveSpeed * Time.fixedDeltaTime;
+
+		//Up
+		if (_currentMovementDirection.y > 0)
+		{
+			var maxY = Mathf.CeilToInt(_rigidbody.position.y);
+			if (nextMovePosition.y >= maxY)
+			{
+				transform.position = new Vector2(_rigidbody.position.x, maxY);
+				moveDistance = nextMovePosition.y - maxY;
+			}
+		}
+		//Left
+		if (_currentMovementDirection.x < 0)
+		{
+			var minX = Mathf.FloorToInt(_rigidbody.position.x);
+			if (nextMovePosition.x <= minX)
+			{
+				transform.position = new Vector2(minX, _rigidbody.position.y);
+				moveDistance = minX - nextMovePosition.x;
+			}
+		}
+		//Down
+		if (_currentMovementDirection.y < 0)
+		{
+			var minY = Mathf.FloorToInt(_rigidbody.position.y);
+			if (nextMovePosition.y <= minY)
+			{
+				transform.position = new Vector2(_rigidbody.position.x, minY);
+				moveDistance = minY - nextMovePosition.y;
+			}
+		}
+		//Right
+		if (_currentMovementDirection.x > 0)
+		{
+			var maxX = Mathf.CeilToInt(_rigidbody.position.x);
+			if (nextMovePosition.x >= maxX)
+			{
+				transform.position = new Vector2(maxX, _rigidbody.position.y);
+				moveDistance = nextMovePosition.x - maxX;
+			}
+		}
+
+		Physics2D.SyncTransforms();
+
+		if (_rigidbody.position.x == Mathf.CeilToInt(_rigidbody.position.x) && _rigidbody.position.y == Mathf.CeilToInt(_rigidbody.position.y))
+		{
+			if (_currentMovementDirection != _desireMovementDirection)
+			{
+				if (!Physics2D.BoxCast(_rigidbody.position, _boxSize, 0, _desireMovementDirection, 1f, 1 << LayerMask.NameToLayer("Level")))
+				{
+					_currentMovementDirection = _desireMovementDirection;
+					OnDirectionCharged?.Invoke(CurrentMoveDirection);
+				}
+			}
+
+			if (Physics2D.BoxCast(_rigidbody.position, _boxSize, 0, _desireMovementDirection, 1f, 1 << LayerMask.NameToLayer("Level")))
+			{
+				_currentMovementDirection = Vector2.zero;
+				OnDirectionCharged?.Invoke(CurrentMoveDirection);
+			}
+
+		}
+
+		_rigidbody.MovePosition(_rigidbody.position + _currentMovementDirection * moveDistance);
+	}
+}
---
+++ Assets/Scripts/PacManInput.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMotor))]
+public class PacManInput : MonoBehaviour
+{
+	[SerializeField]
+	private CharacterMotor _motor;
+
+	void Start()
+	{
+		_motor.GetComponent<CharacterMotor>();
+	}
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			_motor.SetMoveDirection(Direction.Up);
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			_motor.SetMoveDirection(Direction.Left);
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			_motor.SetMoveDirection(Direction.Down);
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			_motor.SetMoveDirection(Direction.Right);
+		}
+	}
+}
